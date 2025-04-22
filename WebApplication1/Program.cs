72ca3050fe535ac5db1da566e4236a2e09afbb11
sai <sai@gmail.com>
2025-04-22 15:10:44 +0530

    ggg

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -10,7 +10,7 @@ builder.Services.ConfigureHttpJsonOptions(options =>
 var app = builder.Build();
 
 var sampleTodos = new Todo[] {
-    new(1, "Walk the dog"),
+    new(1, "Walk tyyhe dog"),
     new(2, "Do the dishes", DateOnly.FromDateTime(DateTime.Now)),
     new(3, "Do the laundry", DateOnly.FromDateTime(DateTime.Now.AddDays(1))),
     new(4, "Clean the bathroom"),
